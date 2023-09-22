7698f40c0d3817ce343caae54c609fce3b52ed96
Basdlwaschdl <ftpobi@googlemail.com>
2023-09-22 21:48:57 +0200

    add da-bubble project

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -17,9 +17,9 @@ export class PortfolioComponent {
   };
   projects = {
     name: ['Join', 'El Pollo Loco', 'Simple CRM', 'Ring of fire'],
-    image: ['join photo.png', 'Pollo loco.png', 'Simple CRM photo.png', 'ring.png'],
-    languages: ['Angular | TypeScript | HTML | CSS | Firebase', 'Javascript | HTML | CSS', 'Angular | Firebase', 'Angular | Typescript | HTML | CSS | Firebase'],
-    description: ['Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.', 'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.', 'A very Simple Customer Relationship Management system working with CRUD functionality. (coming soon !!)', 'Inspired by the card game "Ring of fire", the application provides a great platform to play the game with friends located away from you. Just insert the name, select your icon character and pull the card. Always serve alcohol with moderation!'],
+    image: ['join photo.png', 'Pollo loco.png', 'DA-Bubble.png', 'ring.png'],
+    languages: ['Angular | TypeScript | HTML | CSS | Firebase', 'Javascript | HTML | CSS', 'Angular | TypeScript | HTML | CSS | Firebase', 'Angular | Typescript | HTML | CSS | Firebase'],
+    description: ['Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.', 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and bottles to fight against the killer chicken.', 'DA-Bubble is a team communication platform that simplifies group messaging and collaboration. It allows teams to communicate in real-time and share files efficiently.', 'Inspired by the card game "Ring of fire", the application provides a great platform to play the game with friends located away from you. Just insert the name, select your icon character and pull the card. Always serve alcohol with moderation!'],
   }
 
 
@@ -45,12 +45,14 @@ export class PortfolioComponent {
   openSite(i: number) {
     if (i == 0) window.open('https://join.silvio-schriefl.de/', '_blank')
     if (i == 1) window.open('https://el-pollo-loco.silvio-schriefl.de/', '_blank')
+    if (i == 2) window.open('https://da-bubble.silvio-schriefl.de/', '_blank')
     if (i == 3) window.open('https://ring-of-fire.silvio-schriefl.de/', '_blank')
   }
 
   openGit(i: number) {
     if (i == 0) window.open('https://github.com/SilvioSchriefl/join.git', '_blank')
     if (i == 1) window.open('https://github.com/SilvioSchriefl/El-Pollo-Loco.git', '_blank')
+    if (i == 2) window.open('https://github.com/SilvioSchriefl/DABubble.git', '_blank')
     if (i == 3) window.open('https://github.com/SilvioSchriefl/Ring-of-Fire.git', '_blank')
   }
 }
